c801f3244c6c35acd80c9775c77e610fedccc637
mikeascalon <mikeascalon@yahoo.com>
2024-01-09 15:55:58 -0800

    fibonacci test passed

---
+++ math_series/series.py
@@ -0,0 +1,11 @@
+def fibonacci(num):
+    if num == 0: 
+        return 0
+    
+    if num == 1:
+        return 1
+    
+    a, b = 0, 1
+    for _ in range(2, num + 1):
+        a, b = b, a + b
+    return b
---
+++ tests/test_series.py
@@ -0,0 +1,26 @@
+from math_series.series import fibonacci
+
+def test_fibonacci_zero():
+    actual = fibonacci(0)
+    expected = 0
+    assert actual == expected
+
+def test_fibonacci_one():
+    actual = fibonacci(1)
+    expected = 1
+    assert actual == expected
+
+def test_fibonacci_two():
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
+
+def test_fibonacci_three():
+    actual = fibonacci(3)
+    expected = 2
+    assert actual == expected
+
+def test_fibonacci_four():
+    actual = fibonacci(3)
+    expected = 2
+    assert actual == expected
