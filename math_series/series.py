c1ba5507427e8cb5c3178f9854afa2e65d9cad4e
mikeascalon <mikeascalon@yahoo.com>
2024-01-09 16:14:58 -0800

    lucas pass test

--- math_series/series.py
+++ math_series/series.py
@@ -9,3 +9,15 @@ def fibonacci(num):
     for _ in range(2, num + 1):
         a, b = b, a + b
     return b
+
+def lucas(num):
+    if num == 0:
+        return 2
+    
+    if num == 1:
+        return 1
+    
+    a, b = 2, 1
+    for _ in range(2, num + 1):
+        a, b = b, a + b
+    return b
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,4 +1,4 @@
-from math_series.series import fibonacci
+from math_series.series import fibonacci, lucas
 
 def test_fibonacci_zero():
     actual = fibonacci(0)
@@ -24,3 +24,23 @@ def test_fibonacci_four():
     actual = fibonacci(3)
     expected = 2
     assert actual == expected
+
+def test_lucas_zero():
+    actual = lucas(0)
+    expect = 2
+
+def test_lucas_one():
+    actual = lucas(1)
+    expect = 1
+
+def test_lucas_three():
+    actual = lucas(3)
+    expect = 3
+
+def test_lucas_four():
+    actual = lucas(4)
+    expect = 4
+    
+def test_lucas_five():
+    actual = lucas(5)
+    expect = 7
